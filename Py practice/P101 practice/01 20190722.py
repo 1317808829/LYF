3a7cadc29c7ff9060c520ee9232bef5d855ed82a
1317808829 <52646753+1317808829@users.noreply.github.com>
2019-07-22 09:50:37 +0800

    完工

--- Py practice/P101 practice/01 20190722.py
+++ Py practice/P101 practice/01 20190722.py
@@ -7,3 +7,28 @@ print(a / b)
 print(a // b)
 print(a % b)
 print(a ** b)
+print('hello world!')
+
+print('hello','world','and','girls', sep=',', end='!')
+print('goodbye,world',end='!\n')
+    #这个是注释
+    """
+    这个是
+    三行
+    注释
+    """
+
+import this
+
+import turtle
+    
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
